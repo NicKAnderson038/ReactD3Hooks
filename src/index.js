80ed5786f3a57c3f58c34324cd2371cfac54e7c3
Nick Anderson <nickanderson@gmail.com>
2019-04-03 12:41:05 -0700

    ReactD3Hooks
    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import * as d3 from "d3";
+import PieHooks from "./PieHooks";
 
 import "./styles.css";
 
 function App() {
+  const generateData = (value, length = 5) =>
+    d3.range(length).map((item, index) => ({
+      date: index,
+      value: value === null || value === undefined ? Math.random() * 100 : value
+    }));
+
+  const [data, setData] = useState(generateData(0));
+  const changeData = () => {
+    setData(generateData());
+  };
+
+  useEffect(
+    () => {
+      setData(generateData());
+    },
+    [!data]
+  );
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <button onClick={changeData}>Transform</button>
+      </div>
+      <div>
+        <span className="label">Animated Pie SVG: Hooks</span>
+        <PieHooks
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
     </div>
   );
 }
